bb37377da7670fc65246c8e66ecefcbd59b2d909
Jacinto Escobar Quezada <eqjo190657@upemor.edu.mx>
2024-01-02 19:04:53 -0600

    Se agrega la funcionalidad de consultar las pólizas al dar click en Polizas en el menú principal

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -3,27 +3,37 @@ import "./menu.css";
 
 const Menu = () => {
     return (
-        <ul id="menu" className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-                <Link to="/polizas" className="nav-link" aria-current="page">
-                    Polizas
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link
-                    to="/inventarios"
-                    className="nav-link"
-                    aria-current="page"
-                >
-                    Inventarios
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link to="/empleados" className="nav-link" aria-current="page">
-                    Empleados
-                </Link>
-            </li>
-        </ul>
+        <nav className="navbar navbar-expand-lg">
+            <ul id="menu" className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                    <Link
+                        to="/polizas"
+                        className="nav-link"
+                        aria-current="page"
+                    >
+                        Polizas
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link
+                        to="/inventarios"
+                        className="nav-link"
+                        aria-current="page"
+                    >
+                        Inventarios
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link
+                        to="/empleados"
+                        className="nav-link"
+                        aria-current="page"
+                    >
+                        Empleados
+                    </Link>
+                </li>
+            </ul>
+        </nav>
     );
 };
 
--- src/components/TablePolizas/TablePoliza.jsx
+++ src/components/TablePolizas/TablePoliza.jsx
@@ -1,5 +1,79 @@
+import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
+import "./table-polizas.css";
+
 const TablePolizas = () => {
-    return <h2>Pólizas</h2>;
+    const [polizas, setPolizas] = useState(null);
+
+    const getPolizas = async () => {
+        try {
+            const data = await fetch("http://localhost:8080/poliza", {
+                method: "GET",
+            }).then((response) => response.json());
+            setPolizas(data.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        getPolizas();
+    }, []);
+
+    return (
+        <>
+            <section id="menu-polizas">
+                <button className="btn btn-primary btn-sm" id="nuevo-polizas">
+                    <FontAwesomeIcon icon={faPlus} />
+                </button>
+                <h2 id="titulo-polizas">Pólizas</h2>
+            </section>
+            <table id="table-polizas" className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Cantidad</th>
+                        <th scope="col">Fecha</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {polizas ? (
+                        polizas.map((poliza) => {
+                            return (
+                                <tr key={poliza.idPoliza}>
+                                    <th scope="row">{poliza.idPoliza}</th>
+                                    <td>{poliza.cantidad}</td>
+                                    <td>{poliza.fecha}</td>
+                                    <td>
+                                        <button
+                                            id="button-editar"
+                                            className="btn btn-warning btn-sm"
+                                        >
+                                            <FontAwesomeIcon icon={faPencil} />
+                                        </button>
+                                        <button
+                                            id="button-eliminar"
+                                            className="btn btn-danger btn-sm"
+                                        >
+                                            <FontAwesomeIcon icon={faTrash} />
+                                        </button>
+                                    </td>
+                                </tr>
+                            );
+                        })
+                    ) : (
+                        <tr>
+                            <td colSpan={4} align="center">
+                                Cargando datos...
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </>
+    );
 };
 
 export default TablePolizas;
